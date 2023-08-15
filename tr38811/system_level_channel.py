6b9fbdb245afb77a89af74a34cd6c378392714f0
timduee <timduee@uni-bremen.de>
2023-08-15 15:10:54 +0200

    adapted system level channel for examples

--- tr38811/system_level_channel.py
+++ tr38811/system_level_channel.py
@@ -54,6 +54,7 @@ class SystemLevelChannel(ChannelModel):
 
         self._scenario = scenario
         self._lsp_sampler = LSPGenerator(scenario)
+        print("the smapler is: ", self._lsp_sampler)
         self._ray_sampler = RaysGenerator(scenario)
         self._set_topology_called = False
 
